a4aa7fe63a4824713a5dea74ca4c6656de69c9c5
LadyBethJ <maribel.juradobaena@gmail.com>
2021-06-07 20:17:48 +0200

    made CharacterDetailView parcelable

--- app/src/main/java/com/mjb/mymarvelapp/presentation/characterDetail/models/CharacterDetailView.kt
+++ app/src/main/java/com/mjb/mymarvelapp/presentation/characterDetail/models/CharacterDetailView.kt
@@ -1,7 +1,11 @@
 package com.mjb.mymarvelapp.presentation.characterDetail.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class CharacterDetailView(
     val name: String,
     val image: String,
     val description: String
-)
+) : Parcelable
